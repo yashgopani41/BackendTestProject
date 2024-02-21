130c686433b3f0bcf8682831831fb90d0519ddee
yashgopani41 <155447386+yashgopani41@users.noreply.github.com>
2024-02-21 11:49:54 +0530

    comment ,like , playlist,tweet models added and remove image

---
+++ src/models/playlist.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const playlistSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+        required: true,
+      },
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
